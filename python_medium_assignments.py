a1b344da77c2648183ad7624717b8f487de04a86
Jacob Deel <jacoblakedeel@gmail.com>
2020-11-11 16:55:34 -0500

    Medium Assignments

---
+++ python_medium_assignments.py
@@ -0,0 +1,67 @@
+#Assignment 1
+
+# bill_amount = float(input("How much was the bill? "))
+# service_grade = input("How was the service? Good, Fair, or Bad? ").strip().lower()
+
+# if service_grade == "good":
+#     tip_amount = .20
+# elif service_grade == "fair":
+#     tip_amount = .15
+# elif service_grade == "bad":
+#     tip_amount = .10
+# else:
+#     print("Please describe your service.")
+
+# print(f"Tip amount: {tip_amount}")
+
+# total_bill = bill_amount + tip_amount
+
+# print(f"Total Bill: {total_bill}")
+
+
+
+#Assignment 2
+
+# bill_amount = float(input("How much was the bill? "))
+# service_grade = input("How was the service? Good, Fair, or Bad? ").strip().lower()
+# split_ways = int(input("How many ways would you like to split the check? "))
+
+# if service_grade == "good":
+#     tip_amount = .20
+# elif service_grade == "fair":
+#     tip_amount = .15
+# elif service_grade == "bad":
+#     tip_amount = .10
+# else:
+#     print("Please describe your service.")
+
+# print(f"Tip amount: {tip_amount}")
+
+# total_bill = bill_amount + tip_amount
+# amount_per_person = total_bill/split_ways
+
+# print(f"Total Bill: {total_bill} \nAmount per person: {amount_per_person}")
+
+
+#Assignment 3
+
+# user_coins = 0
+# user_answer = (input("Would you another coin? "))
+
+# while user_answer == "yes".lower():
+#     user_coins += 1
+#     print(f"Your coin total is now: {user_coins} coins")
+#     user_answer = input("Another? ")
+# else:
+#     print("Complete")    
+
+
+#Assignment 4
+
+print("Time to build a box!")
+
+height = input("How high would you like your box?")
+width = input("How wide would you like your box?")
+
+
+
