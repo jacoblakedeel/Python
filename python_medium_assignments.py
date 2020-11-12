19f7a9519316dd628d4d43c569ab28187156a6c4
Jacob Deel <jacoblakedeel@gmail.com>
2020-11-12 08:18:18 -0500

    Didn't quite complete

--- python_medium_assignments.py
+++ python_medium_assignments.py
@@ -1,4 +1,4 @@
-#Assignment 1
+#ASSIGNMENT 1: TIP CALCULATOR
 
 # bill_amount = float(input("How much was the bill? "))
 # service_grade = input("How was the service? Good, Fair, or Bad? ").strip().lower()
@@ -20,7 +20,7 @@
 
 
 
-#Assignment 2
+#ASSIGNMENT 2: SPLIT CHECKS
 
 # bill_amount = float(input("How much was the bill? "))
 # service_grade = input("How was the service? Good, Fair, or Bad? ").strip().lower()
@@ -43,7 +43,9 @@
 # print(f"Total Bill: {total_bill} \nAmount per person: {amount_per_person}")
 
 
-#Assignment 3
+
+
+#ASSIGNMENT 3: COINS
 
 # user_coins = 0
 # user_answer = (input("Would you another coin? "))
@@ -56,12 +58,73 @@
 #     print("Complete")    
 
 
-#Assignment 4
-
-print("Time to build a box!")
-
-height = input("How high would you like your box?")
-width = input("How wide would you like your box?")
 
 
+#ASSIGNMENT 4: BUILD A BOX (ALMOST)
 
+# print("Time to build a box!")
+
+# height = int(input("How high would you like your box? "))
+# width = int(input("How wide would you like your box? "))
+
+# top = ("* " * width)
+# side = ("\b*\n " * height)
+# second_side = (width * " *")
+# bottom = ("* " * width)
+# print(top, side, bottom, second_side)
+
+
+
+
+#ASSIGNMENT 5: TRIANGLE
+
+#I do not understand this problem. Found this solution online.
+
+# print("Now let's build a triangle!")
+
+# n = 0
+# r = 6
+# for m in range(1, r+1):
+#     for gap in range(1, (r-m)+1):
+#         print(end="  ")
+#     while n != (2*m-1):
+#         print("* ", end="")
+#         n += 1
+#     n = 0
+#     print()
+
+
+
+#ASSIGNMENT 6: MULTIPLICATION TABLE
+
+# num = 1
+# num2 = 2
+# num3 = 3
+# num4 = 4
+# num5 = 5
+# num6 = 6
+# num7 = 7
+# num8 = 8
+# num9 = 9
+# num10 = 10
+
+# for i in range(1, 11):
+#    print(num, 'x', i, '=', num*i)
+# for i in range(1, 11):
+#    print(num2, 'x', i, '=', num2*i)
+# for i in range(1, 11):
+#    print(num3, 'x', i, '=', num3*i)
+# for i in range(1, 11):
+#    print(num4, 'x', i, '=', num4*i)
+# for i in range(1, 11):
+#    print(num5, 'x', i, '=', num5*i)
+# for i in range(1, 11):
+#    print(num6, 'x', i, '=', num6*i)
+# for i in range(1, 11):
+#    print(num7, 'x', i, '=', num7*i)
+# for i in range(1, 11):
+#    print(num8, 'x', i, '=', num8*i)
+# for i in range(1, 11):
+#    print(num9, 'x', i, '=', num9*i)
+# for i in range(1, 11):
+#    print(num10, 'x', i, '=', num10*i)
