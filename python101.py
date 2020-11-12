64acde325709c9ff64a2b2d095f4f16c27674b7a
Jacob Deel <jacoblakedeel@gmail.com>
2020-11-12 08:18:55 -0500

    Almost Got It

---
+++ python101.py
@@ -0,0 +1,70 @@
+#Small Tasks
+
+#Assignment 1
+
+# name = input("What is your name? ")
+# print("Hello " + name + "!")
+
+#Assignment 2
+
+# name = (input("What is your name? ").upper())
+# length = len(name)
+# print("Hello " + name + "! Your name has " + str(length) + " letters in it!")
+
+#Assignment 3
+
+# print("Please fill in the following MadLib: ___'s favorite album of all time is ___.")
+# name = input("What is your name? ")
+# fav_album = input("What is your favorite album? ")
+# print(f"{name}'s favorite album of all time is {fav_album}.")
+
+#Assignment 4
+
+# day = int(input("Day (0-6)? "))
+# if day == 0 :
+#     print("Sunday")
+# elif day == 1 :
+#     print("Monday")
+# elif day == 2 :
+#     print("Tuesday")
+# elif day == 3 :
+#     print("Wednesday")
+# elif day == 4 :
+#     print("Thursday")
+# elif day == 5 :
+#     print("Friday")
+# else:
+#     print("Saturday")
+
+#Assignment 5
+
+# day_of_week = input("What day of the week is it? ")
+# if day_of_week == "Monday":
+#     print("Go to work!")
+# elif day_of_week == "Tuesday":
+#     print("Go to work!")
+# elif day_of_week == "Wednesday":
+#     print("Go to work!")
+# elif day_of_week == "Thursday":
+#     print("Go to work!")
+# elif day_of_week == "Friday":
+#     print("Go to work!")
+# elif day_of_week == "Saturday":
+#     print("Sleep in!")
+# else:
+#     print("Sleep in!")
+
+#Assignment 6
+
+# celsius = int(input("Please enter a temperature in Celsius: "))
+# print((celsius * 9/5) + 32)
+
+#Assignment 7
+
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+#Assignment 8
+
