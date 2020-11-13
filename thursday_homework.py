375f4932e6a531068c390e62945eba2dfe7df7c6
Jacob Deel <jacoblakedeel@gmail.com>
2020-11-12 20:16:10 -0500

    Getting Better

---
+++ thursday_homework.py
@@ -0,0 +1,149 @@
+#SMALL ASSIGNMENTS
+
+#ASSIGNMENT 1
+
+# sum = 0
+# myList = [2, 4, 6, 8, 10]
+
+# for nums in range(0, len(myList)):
+#     sum = sum + myList[nums]
+
+# print(sum)
+
+
+#ASSIGNMENT 2
+
+# myList2 = [16, 19, 200, 36, 70]
+
+# largest = max(myList2)
+
+# print(f"The largest number is: {largest}")
+
+
+#ASSIGNMENT 3
+
+# myList3 = [1200, 34, 68, 123, 45]
+
+# smallest = min(myList3)
+
+# print(f"The smallest number is: {smallest}")
+
+
+#ASSIGNMENT 4
+
+
+# mylist4 = [17, 34, 56, 33, 89]
+
+
+# for x in range(5):
+#     if (mylist4[x] % 2 == 0):
+#         print(mylist4[x])
+    
+
+    
+#ASSIGNMENT 5
+
+# mylist5 = [-2, 3, 76, -89]
+
+# for pos in range(4):
+#     if mylist5[pos] > 0:
+#         print(mylist5[pos])
+
+
+
+
+#ASSIGNMENT 6
+
+# mylist6 = [67, -58, 90, -102, 300]
+# newlist = []
+
+# for poslist in range(5):
+#     if mylist6[poslist] > 0:
+#         newlist.append(mylist6[poslist])
+
+# print(newlist)
+
+
+#ASSIGNMENT 7
+
+# mylist7 = [16, 37, 68, -35, 102]
+# factor = 3
+
+# newlist2 = []
+
+# for mult in range(5):
+#     newlist2.append(mylist7[mult] * factor)
+    
+    
+
+# print(newlist2)
+
+
+
+
+#ASSIGNMENT 8
+
+
+# myString = "Hey there! I'm a string! There's a theory goin around about me."
+
+# print(list(reversed(myString)))
+
+
+
+
+
+#MEDIUM ASSIGNMENTS
+
+#ASSIGNMENT 1
+
+# nums1 = [2, 68, 10, 12]
+# nums2 = [18, 2, 10, 2]
+
+# result_list = [] 
+# for mult in range(4): 
+#     result_list.append(nums1[mult] * nums2[mult]) 
+
+# print(result_list)
+
+
+#ASSIGNMENT 2
+
+# twoD = [ [4, -9], [8, 3] ]
+# twoD2 = [ [3, -8], [4, 6] ]
+
+
+# result = [[0, 0], [0, 0]]
+
+# for add in range(2):
+#     for add2 in range(2):
+#         result[add][add2] = twoD[add][add2] + twoD2[add][add2]
+
+# for r in result:
+#     print(r)
+
+
+#ASSIGNMENT 3 (Not Complete)
+
+
+# twoD = [ [4, -9], [8, 3] ]
+# twoD2 = [ [3, -8], [4, 6] ]
+
+
+# result = [[0, 0], [0, 0]]
+
+# for add in range(2):
+#     for add2 in range(2):
+#         result[add][add2] = twoD[add][add2] + twoD2[add][add2]
+    
+# for r in result:
+#     print(r)
+
+
+
+#ASSIGNMENT 4
+
+# strings = ["Hello", "Guvnah", "How", "Hello", "Guvnah"]
+
+# newStrings = []
+
+
