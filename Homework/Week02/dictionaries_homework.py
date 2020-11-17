d7e71911727768e14cd083efc04c5e5eebd4a2dc
Jacob Deel <jacoblakedeel@gmail.com>
2020-11-17 10:06:16 -0500

    Updated HW

---
+++ Homework/Week02/dictionaries_homework.py
@@ -0,0 +1,68 @@
+#SMALL 
+
+#1 PHONEBOOK
+
+
+
+# phonebook_dict = {
+#     'Alice': '703-493-1834',
+#     'Bob': '857-384-1234',
+#     'Elizabeth': '484-584-2923'
+# }
+
+# elizabeth_number = phonebook_dict['Elizabeth'][0]
+# print(elizabeth_number)
+
+# phonebook_dict['Kareem'] = '678-989-0987'
+# print(phonebook_dict)
+
+# del phonebook_dict['Alice']
+# print(phonebook_dict)
+
+# phonebook_dict['Bob'] = '654-345-1231'
+# print(phonebook_dict)
+
+# print(phonebook_dict['Bob'])
+# print(phonebook_dict['Elizabeth'])
+# print(phonebook_dict['Kareem'])
+
+
+
+
+
+
+#2 NESTED DICTIONARIES
+
+
+# ramit = {
+#     'name': 'Ramit',
+#     'email': 'ramit@gmail.com',
+#     'interests': ['movies', 'tennis'],
+#     'friends': [
+#     {
+#         'name': 'Jasmine',
+#         'email': 'jasmine@yahoo.com',
+#         'interests': ['photography', 'tennis']
+#     },
+#     {
+#         'name': 'Jan',
+#         'email': 'jan@hotmail.com',
+#         'interests': ['movies', 'tv']
+#     }
+#     ]
+# }
+
+# ramit_email = ramit['email']
+# print(ramit_email)
+
+# ramit_interest1 = ramit['interests'][0]
+# print(ramit_interest1)
+
+
+# jasmine_email = ramit['friends'][0]['email']
+# print(jasmine_email)
+
+# jan_int = ramit['friends'][1]['interests'][1]
+# print(jan_int)
+
+
---
+++ Notes/Friday_Notes.py
@@ -0,0 +1,10 @@
+#FUNCTIONS
+
+# def fun_name():
+#     print("Hello " * 3)
+
+# fun_name()
+
+
+
+
